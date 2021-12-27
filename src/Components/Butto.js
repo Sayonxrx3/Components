9faa8945d2c5f04fa94183506173927eff5ee3b9
DESKTOP-VSDT22Q\PER <per_112010@hotmail.com>
2021-12-27 18:04:54 +0700

    Sayo

---
+++ src/Components/Butto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './btn.css';
+
+export default function Butto() {
+    return (
+
+        <a href='https://youtu.be/DuWwLGG5CnU'> 
+            <button className='border-2 text-black bg-pink-200'> SADMAN </button>
+        </a>
+    )
+}
---
+++ src/Components/Inputo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Inputo() {
+    return (
+       
+    <form>
+        
+        <input className='rounded-2xl border-4 bg-purple-300' type="text" name="name" />       
+        <input type="submit" value="กดหนูเลยค่ะพี่" />
+      
+    </form>
+
+    )
+}
